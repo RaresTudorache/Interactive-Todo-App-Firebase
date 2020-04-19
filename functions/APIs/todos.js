950116380979311aa55f767245d7ffa74a8080e8
RaresTudorache <rares@RaresLaptop.localdomain>
2020-04-19 13:25:38 +0300

    database configured

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -3,5 +3,23 @@ const {db} = require ('../util/admin');
 
 exports.getAllTodos = (request,response) => {
     db
-       .collection('todos');
-}
+       .collection('todos')
+       .orderBy('createAt', 'desx')
+       .get()
+       .then( data => {
+            let todos = [];
+            data.forEach(doc => {
+                todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    createdAt: doc.data().createAt,
+                });
+            });       
+            return response.json(todos); 
+       })
+       .catch(err => {
+           console.error(err);
+           return response.status(500).json({error:err.code});
+       });
+};
