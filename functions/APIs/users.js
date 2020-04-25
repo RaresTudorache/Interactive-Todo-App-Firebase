8583d72985f7feaf56f0f66aaa9e3218d558aa8f
RaresTudorache <rares@RaresLaptop.localdomain>
2020-04-25 20:38:06 +0300

    authentication added

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -8,4 +8,28 @@ firebase.initializeApp(config);
 const{
     validateLoginData,
     validateSignupData
-} = require('../util/validations');
+} = require('../util/validators');
+
+//login
+
+exports.loginUser = (request,response) => {
+    const user = {
+        email: request.body.email,
+        password: request.body.password
+    }
+
+    const {valid, errors} = validateLoginData(user);
+    if(!valid) return response.status(400).json(errors);
+
+    firebase
+            .auth().signInWithEmailAndPassword(user.email, user.password)
+            .then( (data) => {
+                return data.user.getIdToken();
+            })
+            .then((token)=> {
+                return response.json({token});
+            })
+            .catch((error)=> {
+                return response.status(403).json({general: 'wrong credetinals, please try again'});
+            })
+};
